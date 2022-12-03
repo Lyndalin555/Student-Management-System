dad66cbf064d5bca346bae83b03bbc434e307300
Lyndalin Emekwa <lyndalinemekwa@Lyndalins-MacBook-Air.local>
2022-12-03 22:49:30 +0000

    I just saved my file

---
+++ student2.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE studentDB;
+USE studentDB
+CREATE table studentTable(
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    class VARCHAR(200 NOT NULL,
+    );
+    
+    INSERT INTO studentTable(name, class)
+    values ("mike", "ss1"),
+    ("jude", "ss1"),
+    ("john", "ss1"),
+    ("chidi", "ss3"),
+    ("ada", "jss2");
+
+    SELECT * FROM studentTable;
+    
+
+
+)
